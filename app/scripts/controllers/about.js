de920db5e480aa15dc74e5bb2e8f91b72a616a2d
erinlehmkuhl <erinlehmkuhl@gmail.com>
2015-12-16 10:31:37 -0800

    angular files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name humannApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the humannApp
+ */
+angular.module('humannApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name humannApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the humannApp
+ */
+angular.module('humannApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
